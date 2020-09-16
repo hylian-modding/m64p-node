3e86fe4b950bc0a82fdf6ae7f48517ffae239962
sylvaind1 <17965220+sylvaind1@users.noreply.github.com>
2020-09-16 15:57:09 +0200

    Change memory viewer byte order

--- src/frontend/mem_viewer/view.cpp
+++ src/frontend/mem_viewer/view.cpp
@@ -9,6 +9,23 @@ View::View(M64P::Core& core) :
     m_core{&core}
 {}
 
+static bool s_EditInit = false;
+
+inline std::size_t GetByteOffset(size_t offset)
+{
+    return offset / 4 * 4 + (3 - (offset & 3));
+}
+
+u8 ReadFn(const u8* data, size_t offset)
+{
+    return data[GetByteOffset(offset)];
+}
+
+void WriteFn(u8* data, size_t offset, u8 value)
+{
+    data[GetByteOffset(offset)] = value;
+}
+
 void View::Show(SDL::Window& main_win, ImFont* font_mono)
 {
     if (!m_open)
@@ -16,6 +33,12 @@ void View::Show(SDL::Window& main_win, ImFont* font_mono)
 
     static MemoryEditor dram_edit;
     static MemoryEditor rom_edit;
+
+    if (!s_EditInit) {
+        s_EditInit = true;
+        dram_edit.ReadFn = rom_edit.ReadFn = ReadFn;
+        dram_edit.WriteFn = rom_edit.WriteFn = WriteFn;
+    }
 
     ImGui::SetNextWindowSize({720.0, 500.0f}, ImGuiCond_Appearing);
     ImGui::Begin("Memory viewer", &m_open, GetWindowFlags(main_win) | ImGuiWindowFlags_NoDocking);
